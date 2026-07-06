e7db636249b37f7f331546c1a235485675c4295e
Moteeullah Azmi <moteeullahazmi@gmail.com>
2026-07-06 13:39:05 +0530

    day 3 complete

---
+++ Day 03 Data Types/index.js
@@ -0,0 +1,105 @@
+let age = 25;
+let student = true;
+let name = "azmi";
+
+// console.log(typeof age)
+// console.log(typeof student)
+// console.log(typeof name)
+
+// console.log(typeof 10)
+// console.log(typeof 10.1)
+
+// console.log(10/0)
+
+// console.log("25"+25)
+
+// console.log(typeof NaN);
+
+let name1 = "pushkar";
+let name2 = "himanshu";
+let name3 = "ehteshaam";
+
+// console.log("my first student name " +name1+ "second student name " +name2 + "third student name " +name3);
+
+// console.log(`my first name is ${name1} second name ${name2}, third name is ${name3}`)
+
+// let namestudnet = "pushkar"
+
+// console.log("my first student name " +namestudnet);
+
+// console.log(`my first name is ${namestudnet}`);
+
+// let firstNumber = 100;
+// let secondNumber = "100"
+
+// console.log(typeof firstNumber)
+// console.log(typeof secondNumber)
+
+// let isStudent = false;
+// console.log(typeof isStudent)
+
+// let userName = null;
+// console.log(typeof userName)
+
+let largeNumber = 900719925474099734554n;
+// console.log(typeof largeNumber);
+
+// console.log(typeof Number.MAX_SAFE_INTEGER);
+
+let sum1 = 10n;
+let sum2 = 15n;
+
+// console.log(sum1+sum2);
+
+let id1 = Symbol("rahul");
+let id2 = Symbol("rahul");
+
+// console.log(id1 == id2);
+// console.log(id1 === id2);
+
+// let first ="zero"
+// let second= 0
+// console.log(first ==second) // value copmapre huwaa 0 is true
+// console.log(first === second) // value datatype string ek number false
+
+// zero = 0;
+
+// console.log(typeof 100); // number
+// console.log(typeof "Hello"); // string
+// console.log(typeof true); //boolean
+// console.log(typeof undefined); //undefined
+// console.log(typeof 10n); //bigint
+// console.log(typeof Symbol("id")); //symbol
+// console.log(typeof null) 
+
+// console.log(typeof {})
+
+// console.log(typeof function(){})
+
+
+// premitive
+let a =10;
+let b=a;
+
+b= 20;
+
+console.log(a)
+
+
+//refrence
+let user1= {
+    name: "rahul"
+};
+
+let user2=user1;
+
+user2.name="Aman"
+
+console.log(user1.name)
+
+
+let age1 =25;
+let age2 = "25"; //25
+
+console.log(age1==age2);
+
