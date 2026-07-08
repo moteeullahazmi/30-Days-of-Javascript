f3b76a5857126f372370243dd5fd36e57d596185
Moteeullah Azmi <moteeullahazmi@gmail.com>
2026-07-08 11:24:13 +0530

    day 02 complete

---
+++ Day 02 Variable/index.js
@@ -0,0 +1,23 @@
+// let variableName = value;
+
+// let student1 = "Himanshu";
+// let student2 = "Ehteshaam"
+
+{
+    // blockscope
+}
+
+// {
+//     let age =20;
+//     console.log(age)
+// }
+
+// console.log(age)
+
+
+// var is a function scope
+// let const is block scope
+
+
+// {} it is block scope
+
