e9fda1df9681c59302b2e3f3ae223c5742e3fae1
Moteeullah Azmi <moteeullahazmi@gmail.com>
2026-07-08 12:32:19 +0530

    day 04 complete

---
+++ Day 04 Operators/index.js
@@ -0,0 +1,86 @@
+//arithmetic operator
+
+// let a = 10;
+// let b = 6;
+
+// console.log(a+b);  //16
+// console.log(a - b); // 4
+// console.log(a * b); //
+// console.log(a / b);
+// console.log(a + b);
+// console.log(a % b);  //4
+// console.log(a ** b);   // 10 power 6
+
+// console.log(8%2);
+
+// let num = 10;
+
+// console.log(num%2 == 0, "even");
+
+//assignment operator
+
+// let x = 10; //=
+
+// x %= 5; // score= score+5
+
+// console.log(x)
+
+// console.log(10 > 5);  // true
+// console.log(10 < 5);  // false
+// console.log(10 >= 10); // true
+// console.log(10 <= 9);    //false
+
+// console.log("7" !== 7);  // ===
+
+// console.log(9 !== 7);
+
+// let x= "6";
+// let y = 6;
+
+// console.log(typeof x)
+// console.log()
+
+// console.log(!true);
+// console.log(!false);
+
+// let count = 5;
+
+// count++;
+// count += 1  //count = count +1
+
+// console.log(count);
+
+// let x = 5;
+// let y = x--;
+
+// console.log(x);
+// console.log(y);
+
+// let age =20;
+
+// let result =  age= age%2==0 ? "even" : "odd"
+
+// console.log(result)
+
+// let marks = 35;
+
+// let result = marks >= 40 ? "Pass" : "Fail";
+
+// console.log(result)
+
+// console.log((10+2)*3+ 6**2)  // 12*3+ 36 // 36 +36
+
+
+// console.log( "7" = 7)
+
+
+let a = 6;
+let b = a++;
+
+console.log(a,b) //7,6
+
+console.log(b)
+
+b = ++a;
+console.log(b)
+console.log(a,b) //8, 
