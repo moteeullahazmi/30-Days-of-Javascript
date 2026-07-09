af618f3dd0b5b8cca551b26dd8fb6c401256ec81
Moteeullah Azmi <moteeullahazmi@gmail.com>
2026-07-09 13:08:16 +0530

    day 05 completed

---
+++ Day 05 Conversion/index.js
@@ -0,0 +1,80 @@
+let age = "22";
+
+// console.log(typeof age); 
+
+// console.log(typeof Boolean(age))
+
+
+// console.log(typeof ("25" - 2))  //23
+// console.log(typeof ("25" + 3));  // 252   // "25" + "3"
+
+
+// let age ="17";
+
+// newAge = Number(age);
+
+// console.log(typeof age)
+// console.log(typeof newAge)
+
+
+// Number("10");  //10
+// Number(" ");  // 0
+// Number(true);  //1
+// Number(false); //0
+// Number(null);  // 0
+// Number(undefined); //NaN
+// Number("hello");  //NaN
+
+// let num = "aaaa";
+// console.log(typeof String(num));
+
+
+// //boolean
+// console.log(Boolean("hello"));
+// console.log(Boolean(""));
+
+//
+
+// console.log(false);
+// console.log(Boolean("0"));
+// console.log(Boolean(-10));
+// console.log(Boolean(0n));
+// console.log(Boolean(""));
+// console.log(Boolean(null));
+// console.log(Boolean(undefined));
+// console.log(Boolean(NaN));
+
+// console.log(Boolean([]));
+// console.log(Boolean({}))
+
+// console.log(typeof NaN)
+
+console.log(Number.isNaN(NaN));
+
+
+
+console.log("10" + 5);  // 105
+
+console.log("10" - 5); // 5
+
+console.log("10" * 2); // 20
+
+console.log(Number("25")); //25
+
+console.log(Number("hello")); //NaN
+
+console.log(Boolean(0));  //false
+
+console.log(Boolean("0"));  // true
+
+console.log(Boolean(""));  //false
+
+console.log(Boolean("false")); //true
+
+console.log(Boolean([]));  //true
+
+console.log(typeof NaN);  //number
+
+console.log(NaN === NaN);  //false
+
+console.log(NaN == NaN);    //
