0e5e8b3e9d91573bd52efae707c5728435624381
Moteeullah Azmi <moteeullahazmi@gmail.com>
2026-07-10 12:34:11 +0530

    day 06 complete

--- Day 06 Conditional Statements/index.js
+++ Day 06 Conditional Statements/index.js
@@ -1,7 +1,120 @@
-if ([]) {
-  console.log("Array is truthy");
-}
+// if(condition){
+//     //statement
+// } else {
+//statement }
 
-if ({}) {
-  console.log("Object is truthy");
-}
+// let age =18;
+
+// if(age >= 18){
+//     console.log("Adult")
+// } else {
+//     console.log("not Adult")
+// }
+
+// //if{}
+// else if{}
+
+// let marks =59;
+// if(marks >=90) {  //false
+//     console.log( "Grade A");
+// }
+// else if (marks >=60) {  //false
+//     console.log("Grade B")
+// } else {
+//     console.log("Fail")  //fail
+// }
+
+// switch(param){
+//     case 1:
+//     case 2:
+//     case 3:
+
+//     case n:
+//         default:
+// }
+
+// let day ="Monday";
+
+// switch(day){
+//     case "Monday":
+//     console.log(1);
+
+//     case 2:
+//     console.log("Tuesday");
+
+//     case 3:
+//     console.log("Wednesday");
+
+//     case 4:
+//     console.log("Thursday");
+
+//     case 5:
+//         console.log("Friday")
+//         break;
+//     case 6:
+//     console.log("Saturday");
+
+//     case 7:
+//     console.log("Sunday");
+
+//     default: console.log("Wrong day you are enter")
+//     }
+
+// let color = "y";
+
+// switch (color) {
+//   case "r":
+//     console.log("red");
+//     break;
+
+//   case "g":
+//     console.log("green");
+//     break;
+
+//   case "b":
+//     console.log("blue");
+//     break;
+
+//     default: 
+//     console.log("wrong color")
+// }
+
+
+
+
+// let isLogin= false;
+// let isAdmin= true;
+
+// if(isLogin){ //false
+//     if(isAdmin){
+//         console.log("yes i have permission to change my password ")
+//     }
+// }
+
+// if(age >=18){
+//     if(isLicense){
+//         console.log("You can drive")
+//     }
+// }
+
+// if(age >=18 && isLicense){
+//     console.log("You can drive");
+// }
+
+// ternary operator
+
+// condtion ? trueValue : falseValue;
+
+let age =25;
+
+// if(age >=18) {
+//     console.log("you are elgible for voter");
+// } else {
+//     console.log("You are not eligible");
+// }
+
+ let result = age>=18 ? "YOu are eligible" : " You are not eligible"
+
+
+ console.log(result)
+
---
+++ Day 06 Conditional Statements/question.js
@@ -0,0 +1,11 @@
+let age = 1;
+
+switch (age){
+    case 1 :
+        console.log("Number")
+        break;
+    
+    case "1":
+        console.log("string");
+        break;
+}
