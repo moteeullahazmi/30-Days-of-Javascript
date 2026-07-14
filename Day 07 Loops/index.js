e039b7accea31fbd58e42af74862e910480cbf77
Moteeullah Azmi <moteeullahazmi@gmail.com>
2026-07-14 11:57:31 +0530

    day 07

---
+++ Day 07 Loops/index.js
@@ -0,0 +1,78 @@
+// for (
+//   i = 1;
+//   i <= 5;
+//   i++ //i=6
+// ) {
+//   console.log(i); //1,2,3,4,5
+// }
+
+// console.log("The value of", i); // string number
+// console.log("The value of" + i); //string
+
+// // "azmi"5
+
+// // for (initalization; condition; increament){
+
+// }
+
+// for(i=1; i<=100; i++){
+
+//     if(i%2==0){
+//         console.log(i)
+//     }
+// }
+
+// // 2,4,6,8,10
+
+// for (i=10; i>=1;i--){
+//     console.log(i)
+// }
+
+// for loop range ptaa ho 1-100
+// while only condition ptaa ho
+// do-while pahle ek print line uske condition check ho p
+
+// let i = 1;
+// while (i <= 5) {
+//   i++;
+//   console.log(i);
+// }
+
+
+// let i = 1;
+// while (i<=2) {
+   
+//   console.log(i);
+//   i++;
+ 
+// }
+
+// let i=1 ;
+// do{
+//     console.log(i)
+//     i++;
+    
+// } while(i<=5)
+
+
+// let i=10;
+// // while(i<5) {
+// //     console.log(i)
+// // }
+
+// do{
+// console.log(i)
+// i++;
+// } while(i<5)
+
+//     console.log(i)
+
+
+// for (let i=1; i<=5; i++){
+
+//     if(i==3){
+//        continue;  //1,2,4,5
+//     }
+
+//     console.log(i); 
+// }
