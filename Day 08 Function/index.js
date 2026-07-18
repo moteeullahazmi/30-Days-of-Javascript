e5cd9f20a7e1e689833e1a23686dfb4c468109cc
Moteeullah Azmi <moteeullahazmi@gmail.com>
2026-07-18 12:34:14 +0530

    day 08 complete

--- Day 08 Function/index.js
+++ Day 08 Function/index.js
@@ -1,6 +1,90 @@
-function add(a, b) {  
-  console.log(a + b);
-}
+// console.log(10+20);
+// console.log(30+40);
+// console.log(20*30);
 
-const result = add(5, 10);
-console.log(result)
+// function functionName() {
+    // console.log("azmi")
+
+//}
+
+// functionName();
+
+
+// twoNumber(10, 12);
+// twoNumber(20, 29);
+// twoNumber(10, 54);
+
+
+
+// function twoNumber(a,b){
+//   console.log(a + b);
+// }
+
+
+// const sum = function azmi() {
+//   console.log("azmi")
+// }
+
+// sum();
+
+
+// function greet(name="Azmi"){   //parameter
+//   console.log(name)
+// }
+
+// greet("Pushkar")   //argument
+// greet();
+
+
+
+// function add(a,b){
+//    console.log("a+bi ki value ab ayegi");
+//   return a+b;
+ 
+// }
+// const result=add(5,10);
+// console.log("function ke baher hun",result);
+
+
+
+// const varAzmi= function(){
+//   return "azmi"
+// }
+
+// const varAzmi = () => {
+
+// }
+
+
+// const greet = ()=>{
+//   console.log("azmi")
+// }
+
+// greet();
+
+// const add= (a,b) => a+b; //arrow function  (a+b) =parameter  => iskebaad walaa isko bolte hia return    
+
+
+// console.log(add(10,20));
+
+// const greet = function () {
+//   console.log("Hello");
+// };
+
+// const greet= () => "hello";  
+// console.log(greet())
+
+
+
+
+// function sumNumber(a,b){
+//   return a+b;
+// }
+
+// const total = sumNumber(10, 20);
+// console.log(total);
+
+
+const square =(num) => num*num;
+
+console.log(square(5));
