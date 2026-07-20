3d87fafc76ff84a4b2e066b96370b9b76de93483
Moteeullah Azmi <moteeullahazmi@gmail.com>
2026-07-20 12:07:00 +0530

    fay 09

---
+++ Day 09 Scope and Hoisting/index.js
@@ -0,0 +1,102 @@
+// let course = "javascript";  //global scope
+
+// function allCourse() {
+//   console.log(course);  //11
+// }
+
+// allCourse();
+// console.log(course)
+
+// function azmi(){
+//     console.log(course)
+// }
+
+// azmi();
+
+
+// if(true){
+//     var password= "pushkar";
+//     console.log(password)
+// }
+
+
+
+// console.log(password);
+
+
+
+
+// if(true){
+//     var age = 20;
+//     console.log(age)
+// }
+
+// console.log(age)
+
+
+// let course = "linux"
+
+// function first(){
+//     function second(){
+//         console.log(course)
+//     }
+//     second();
+// }
+
+// first();
+
+// const myData = "Global Data";
+
+// function outerFunction() {
+//   const myData = "Outer Function Data";
+
+//   function innerFunction() {
+//     const myData = "Inner Function Data";
+//     console.log(myData); // Which 'myData' prints?
+//   }
+
+//   innerFunction();
+//   console.log(myData); // Which 'myData' prints?
+// }
+
+// outerFunction();
+// console.log(myData); // Which 'myData' prints?
+
+
+// let name = "John"; // Global variable
+
+// function sayHello() {
+//       console.log("Hello " + name);
+// }
+
+// sayHello(); // Output: "Hello John"
+
+
+
+// Hoisting
+
+// const a = 10;  
+// console.log(a);  
+
+
+// sayHello();
+
+// var saHello= function sayHello(){
+//     console.log("Hello")
+// }
+
+// console.log(name);
+
+// let name= "pushkar"
+
+// {
+
+// }
+
+let name = "Moteeullah ";
+function greet() {
+    let name ="Moteeullah "
+}
+
+console.log(name);
+        
