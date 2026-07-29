de3494b52a34aba1d4ecfd51cbb15f229e475d6d
Moteeullah Azmi <moteeullahazmi@gmail.com>
2026-07-29 11:01:52 +0530

    day 10 complete

---
+++ Day 10 String/index.js
@@ -0,0 +1,79 @@
+// let name = "   JAVASCRIPT     ";
+// // console.log(name.length);
+
+// // // upper case
+
+// // console.log(name.toUpperCase())
+// console.log(name.trim());
+
+// let txt= "JavaScript";
+
+// console.log(txt.replace("JavaScript", "Ehtesham"))
+
+// let text = "HTML,CSS,JavaScript";
+// console.log(text.split(","))
+
+// let name = "pushkar"  //k
+// console.log(name.charAt(4))
+
+// let text = "JavaScript";
+
+// console.log(text.indexOf("r"))
+
+// let text="Azmi";
+
+// let azmi = text.toUpperCase();
+
+// console.log(azmi)
+
+// let name1 = "Ehtesham";
+// let name2 = "Pushkar";
+// console.log(name1, "&", name2);
+
+// console.log(`${name1} & ${name2}`);
+
+// let name = "Moteeullah";
+// let age = 25;
+
+// console.log("My name is " + name + " and I am " + age + " years old.");
+
+// console.log(`My name is ${name} and I am ${age} years old `)
+
+
+
+// let message = `hello
+// first name is ehtesham
+// second name is pushkar`
+// console.log(message)
+
+
+
+// let a= 10;
+// let b=20;
+// console.log("sum =", a+b)
+
+// console.log(`Sum = ${a+b}`)
+
+
+// it's
+
+// console.log("D:\\Youtube>");
+
+// hello
+//azmi
+
+// console.log("hello \njavascript")  //\n for new line
+
+// console.log("hello \t azmi \t pushkar \t ehtesham")  //  t\t tab 
+
+// let array = aljdaslkjfl
+// array[0] = a;
+
+// console.log(array)
+
+
+
+let name  = "     javascript and typescript     "
+
+console.log(name.trim().length)
+
