b9e21cb1c8b4d44506037bbe89c77edb4aa7b5e9
Moteeullah Azmi <moteeullahazmi@gmail.com>
2026-07-31 11:10:04 +0530

    day 11

---
+++ Day 11 Number & Math/index.js
@@ -0,0 +1,94 @@
+// console.log(12)
+// console.log(123.44)
+// console.log(-4);
+
+// console.log(10)
+// console.log(-50)
+
+// decimal number
+
+// console.log(120.9)
+// console.log(134.67)
+
+// let a = "20";
+// console.log(typeof a);   ///number
+// console.log(typeof a.toString());  // string
+
+// let inovice = 12345;
+// let result = inovice.toString();
+
+// console.log(typeof result)
+
+// let pi = 3;;
+
+// console.log(pi.toFixed(4));
+
+// let age = "12345bbs3456";
+
+// console.log(parseInt(age));
+
+// let price = "102.12Kg";
+// console.log(parseFloat(price));
+
+// let a= "-12"
+// console.log(parseInt(a))
+
+// console.log(Math.PI);
+
+// let radius = 5;
+
+// let area = Math.PI * radius * radius;
+
+// console.log(area)
+
+// console.log(Math.sqrt(144));
+
+// console.log(Math.pow(2,3)); // 2*2*2
+
+// console.log(Math.abs(-8999.123))
+
+// console.log(Math.min(13,12,80,90));
+
+// console.log(Math.abs(-20));
+
+// console.log(Math.max(12,56,19,90));  //90
+
+// console.log(Math.min(12, 56, 19, 90));  //12
+
+// console.log(Math.round(2.5))
+// console.log(Math.round(2.56));
+// console.log(Math.round(2.4));
+// console.log(Math.round(2.978));
+
+// console.log(Math.floor(2.5));
+// console.log(Math.floor(2.5));
+// console.log(Math.floor(2.4));
+// console.log(Math.floor(2.978));
+
+// console.log(Math.ceil(7.99));
+
+// console.log(Math.trunc(15.94356789))
+
+// let otp = Math.floor(Math.random() * 5) + 8;
+
+// console.log(otp);
+
+// let price = 299.567;
+
+// console.log(price.toFixed(2));
+
+// let balance = "15000";
+
+// console.log(Number(balance));
+
+// let attended = 6;
+// let total = 11;
+
+// let percentage = (attended / total) * 100;
+
+// console.log(percentage.toFixed(2));
+
+
+// console.log(Math.max(25,10,95,62))
+
+console.log(Math.abs(-250))
