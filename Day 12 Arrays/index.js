23dc2062c3aec38e630ffae226cd35c971dcadc5
Moteeullah Azmi <moteeullahazmi@gmail.com>
2026-08-01 10:35:59 +0530

    day 12 complete

---
+++ Day 12 Arrays/index.js
@@ -0,0 +1,101 @@
+// let student1 = "Azmi"
+// let student2 = "Pushkar"
+// let student3 = "Himanshu"
+
+// let students = ["Azmi", "Rahul", "Aman"];
+
+// const shoppingList = ["Milk", "Bread", "Egg", "Rice", "Fruits" ]
+
+// console.log(shoppingList)
+
+// Without array
+// const user1= "aman"
+// const user2 = "Rahul"
+// const user3= "Ali"
+// const user4 = "Sara"
+
+// const users = ["aman", "rahul", "ali", Sara]
+
+
+// const number = [10, 20, 30 ,40]
+
+// const data = [
+//     "Ali",
+//     25,
+//     true,
+//     null
+// ]
+
+// Emoty array
+
+// let students = [];
+
+// const fruits = ["Apple", "Banana", "Mango"]
+
+// fruits[0] = "Orange";
+
+// console.log(fruits);
+
+// const marks = [50,60,70];
+
+//  delete marks[0]
+
+// console.log(marks)
+
+// console.log(marks.length)
+
+
+// // nested array 
+// const data = [
+//     ["pushkar", 30], // 0
+//     ["ehtesham", 25], //1
+//     ["Himanshu", 22]  //2
+// ]
+
+// console.log(data[0][0])
+
+
+// let user1 = "Pushkar";
+// let user2 = "ehteshan"
+
+// let users = ["pushkar", "ehtesham"];
+// console.log(users[2])
+
+
+
+// user1
+// const users= [
+//     ["pushakr", 25], //user1 
+//     ["ehtesham", 22] //user2
+// ]
+
+// console.log(users[0][0])
+
+
+// const fruits = ["apple", "Banana"]
+// delete fruits[0]
+// console.log(fruits)
+// console.log(fruits.length)
+
+// const studentDetails = [
+//     "Pushkar", 25, true, 
+// ]
+
+// const colors = ["Red", "Blue", "Green"];
+
+// console.log(colors[0]);
+
+// const marks = [50,60,70]
+
+// marks[1] = 95;
+
+// console.log(marks)
+
+const users = [
+  ["Ali", 20], //0
+  ["Rahul", 25], // 
+  ["Sara", 22],
+];
+
+
+console.log(users[1][1])
