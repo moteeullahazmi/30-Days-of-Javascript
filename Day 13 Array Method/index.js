c603538223e645a7715590cbb8b61fe5135a7a46
Moteeullah Azmi <moteeullahazmi@gmail.com>
2026-08-04 10:58:18 +0530

    day 13 complete

---
+++ Day 13 Array Method/index.js
@@ -0,0 +1,62 @@
+// let fruits = ["Apple", "Banana"]
+
+// // console.log(fruits)
+
+// // fruits.unshift("Mangoes")
+
+// // console.log(fruits)
+
+// // // splice
+
+// let fruits = ["Apple", "banana", "Mango", "Orange"];
+
+// fruits.splice(1,3); //banana & mango
+
+// console.log(fruits)
+
+//Add
+
+// let fruits = ["Apple", "Orange"];
+
+// fruits.splice(1,1, "Banana", "Mango");
+
+// console.log(fruits);
+
+
+//replace
+
+// let fruits = ["Apple", "Mango","a","b","c"];
+// fruits.splice(2,3, "Kiwi","Orange");
+// console.log(fruits);
+
+
+// let fruits = ["Apple", "Banana", "Mango", "Orange"];
+
+// let result = fruits.slice(2,10);
+// console.log(result);
+
+// console.log(fruits)
+
+
+
+// let frontend = ["HTML", "CSS", "JavaScript", "react.js"];
+
+// let backend = ["node.js", "react.js", "mongodb"]
+
+// let fullstack = frontend.concat(backend);
+
+// console.log(fullstack)
+
+
+
+// let fruits = ["Apple", "Banana", "Mango"];
+
+// console.log(fruits.join(", "))
+
+// let words = ["javascript", "is", "Awesome"];
+// console.log(words.join(" "))
+
+
+let fruits= ["apple", "banana", "mango"];
+
+console.log(fruits.includes("apples"))
