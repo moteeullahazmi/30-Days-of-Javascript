1984352757be053d0c16abf6451533221b0fde0b
Moteeullah Azmi <moteeullahazmi@gmail.com>
2026-08-07 10:32:41 +0530

    day 15 object completed

---
+++ Day 15 Object/index.js
@@ -0,0 +1,111 @@
+// // // const object = {
+// // //     key:values,
+// // //     key1:values
+// // // }
+
+// // const tester1= {
+// //     name: "Rahul",
+// //     experience: 2,
+// //     company: "ABC PVT LTD"
+// // }
+
+// // const tester2 = {
+// //   name: "Pushkar",
+// //   experience: 8,
+// //   company: "XYZ PVT LTD",
+// // };
+
+// // console.log(tester2)
+
+// // delete tester2.name;
+
+// // console.log(tester2)
+
+
+
+// // const tester = {
+// //     name: "Rahul",
+// //     company: {
+// //         name: "OpenAI",
+// //         location: "Banglore"
+// //     }
+// // }
+
+// // console.log(tester.company)
+
+
+// // const config = {
+// //   browser: {
+// //     name: "Chromium",
+// //     headless: true,
+// //   },
+// // };
+
+// // console.log(config.browser.name);
+// // console.log(config.browser.headless);
+
+
+// // const tester = {
+// //     name: "Rahul",
+
+// //     greet() {
+// //         console.log("Welcome Tester ");
+// //     }
+// // }
+
+// // tester.greet();
+
+
+// // const tester = {
+// //     name :"Rahul",
+
+// //     intro(){
+// //         console.log("My name is ", this.name)
+// //     }
+// // }
+
+// // tester.intro();
+
+
+// // const tester134 = {
+// //     name: "Rahul",
+
+// //     intro(){
+// //         console.log(this.name)
+// //     }
+// // }
+
+// // tester134.intro();
+
+
+// const user = {
+//     username: "admin",
+//     password: "admin1234",
+
+//     login(){
+//         console.log( "Login with " + this.username)
+//     }
+// }
+
+// user.login()
+
+// // const arr = ["a", "b", "c" ] 
+
+// const users =[
+//     {name:"azmi", age: 25},
+//     {name: "Pushkar", age: 30},
+//     {name: "Ehtesham",age: 25}
+// ]
+
+// // array of object
+
+// const performanceData = {
+//   months: ["January", "February", "March"],
+//   sales: [10, 15, 8],
+//   expenses: [5, 7, 6],
+// };
+
+// console.log(performanceData.sales[1])
+
+// //object of array
+
