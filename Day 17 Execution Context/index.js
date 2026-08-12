6f3859b3be0555565e7aad7932cc44b7a7ac6eff
Moteeullah Azmi <moteeullahazmi@gmail.com>
2026-08-12 09:44:06 +0530

    day 17 complete

---
+++ Day 17 Execution Context/index.js
@@ -0,0 +1,102 @@
+// let name = "Rahul"
+
+// function greet() {
+//     console.log("Hello");
+// }
+
+// greet();
+
+// // let name ="Rahul";
+
+// // console.log(name)
+
+// // function login() {
+// //   let username = "Rahul";
+// //   console.log(username);
+// // }
+
+// // login();
+
+// let name = "Rahul";
+
+// function greet() {
+//   console.log("Hello");
+// }
+
+// // name= undefine, greet
+
+// let age = 25;
+
+// function test() {
+//   console.log("Testing");
+// }
+
+// console.log(age);
+// test();
+
+// //
+
+// function login() {
+//   console.log("User logged in");
+// }
+
+// login();
+
+// function one() {
+//   console.log("One");
+// }
+
+// function two() {
+//     console.log("two")
+//   one();
+// }
+
+// console.log("Global")
+// two();
+
+// function login() {
+//   console.log("Login");
+// }
+
+// function dashboard() {
+//   login();
+//   console.log("Dashboard");
+// }
+
+// dashboard();
+
+// function enterUsername() {
+//   console.log("Username entered");
+// }
+
+// function enterPassword() {
+//   console.log("Password entered");
+// }
+
+// function login() {
+//   enterUsername();
+//   enterPassword();
+//   console.log("Login clicked");
+// }
+
+// login();
+
+// function testLogin() {
+//   validateUser();
+// }
+
+// function validateUser() {
+//   checkDatabase();
+// }
+
+// function checkDatabase() {
+//   console.log("Database Error");
+// }
+
+// testLogin();
+
+function test(){
+    test();
+}
+
+test()
