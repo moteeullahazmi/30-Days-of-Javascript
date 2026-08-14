c16531dd67bd511b55376e51950178ae01f72eeb
Moteeullah Azmi <moteeullahazmi@gmail.com>
2026-08-14 10:09:16 +0530

    day 19 push origin main

---
+++ Day 19 Calback & Promises/index.js
@@ -0,0 +1,48 @@
+// function task(callback) {
+//   console.log("Kaam start");
+//   callback();  //done();
+// }
+
+// function done() {
+//   console.log("Kaam khatam");
+// }
+
+// task(done);
+
+// // Kaam start 
+// // Kaam Khatam
+
+
+// setTimeout(() => {
+//   console.log("Step 1");
+//   setTimeout(() => {
+//     console.log("Step 2");
+//     setTimeout(() => {
+//       console.log("Step 3");
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+
+let promise = new Promise((resolve, reject) => {
+  let success = true;
+
+  if (success) {
+    resolve("Kaam ho gaya");
+  } else {
+    reject("Error aa gaya");
+  }
+});
+
+
+promise.then((data)=>{
+    console.log(data)
+})
+
+promise.catch((error)=>{
+    console.log(error)
+})
+
+promise.finally(()=>{
+    console.log("Process end")
+})
