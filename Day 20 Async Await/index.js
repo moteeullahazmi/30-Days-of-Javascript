d51174b7a03bd9dc69f7a38be43a9573704b69f4
Moteeullah Azmi <moteeullahazmi@gmail.com>
2026-08-22 09:58:27 +0530

    Day 20 complete

---
+++ Day 20 Async Await/index.js
@@ -0,0 +1,38 @@
+// console.log("Start");
+
+// // for (let i=0; i<100; i++){
+// //     console.log(i)
+// // }
+
+// setTimeout(() => {
+//     console.log("Done")
+// }, 2000);
+
+// console.log("End")
+
+// async function test(){
+//     let data= await Promise.resolve("Hello");
+//     console.log(data)
+// }
+
+// test()
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+// .then(res => res.json())
+// .then(data => console.log(data))
+
+// async function getUsers(){
+//     let res = await fetch("https://jsonplaceholder.typicode.com/users");
+
+//     let data = await res.json()
+//     console.log(data);
+
+// }
+
+// getUsers();
+
+
+// {
+//     "name": "Moteeullah Azmi",
+//     "age" : 20
+// }
