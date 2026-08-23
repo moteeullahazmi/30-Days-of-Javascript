1f348c88fa678e74c03d2795f3c2f8b14a1eb941
Moteeullah Azmi <moteeullahazmi@gmail.com>
2026-08-23 09:59:10 +0530

    day 04 datatypes complete

--- Day 04 Operators/index.js
+++ Day 04 Operators/index.js
@@ -1,112 +1,67 @@
-//arithmetic operator
+// let a =10;
+// let b = 3;
 
-// let a = 10;
-// let b = 6;
+// console.log(a+b);  //13
+// console.log(a - b); // 7
+// console.log(a*b); // 30
+// console.log(a / b); 3.3
+// console.log(a % b); // 1
+// console.log(a ** b); //1000
 
-// console.log(a+b);  //16
-// console.log(a - b); // 4
-// console.log(a * b); //
-// console.log(a / b);
-// console.log(a + b);
-// console.log(a % b);  //4
-// console.log(a ** b);   // 10 power 6
+// let x=5;
+// x +=5; /// x= x+5 > 5+5 = 10
 
-// console.log(8%2);
-
-// let num = 10;
-
-// console.log(num%2 == 0, "even");
-
-//assignment operator
-
-// let x = 10; //=
-
-// x %= 5; // score= score+5
-
-// console.log(x)
-
-// console.log(10 > 5);  // true
-// console.log(10 < 5);  // false
-// console.log(10 >= 10); // true
-// console.log(10 <= 9);    //false
-
-// console.log("7" !== 7);  // ===
-
-// console.log(9 !== 7);
-
-// let x= "6";
-// let y = 6;
-
-// console.log(typeof x)
-// console.log()
-
-// console.log(!true);
-// console.log(!false);
-
-// let count = 5;
-
-// count++;
-// count += 1  //count = count +1
-
-// console.log(count);
+// console.log(10)
 
 // let x = 5;
-// let y = x--;
+// x += 3; //x= x+3  => 5+3 => 8
+// console.log(x); //8
 
-// console.log(x);
-// console.log(y);
+// x = 5;
+// x -= 3;
+// console.log(x); //2
 
-// let age =20;
+// x = 5;
+// x *= 3;
+// console.log(x); //  15
 
-// let result =  age= age%2==0 ? "even" : "odd"
+// x = 5;
+// x /= 3;
+// console.log(x); //1.6
+
+// let a = 5;  //number
+// let b = "5";  // string
+
+// console.log(a == b);  //true
+// console.log(a === b); // false
+// console.log(a != b);  //false
+// console.log(a !== b);  // true
+
+// let age = 20;
+// let hasID=false;
+
+// console.log(age> 18 && hasID);  // true && false => false
+
+// console.log(age>18 || hasID) // true || false => true
+
+// console.log(!hasID)  //true
+
+// let a= "-5";
+// a= -a  // 5
+// console.log(a);
+// console.log(typeof a)
+
+// let x= 10;
+// x--;
+// console.log(x)
+
+// let age =17;
+// let result = age >=18 ? "Adult" : "Not Adult";
 
 // console.log(result)
 
-// let marks = 35;
+let result = 2 + 3 * 4; 
+console.log(result);
 
-// let result = marks >= 40 ? "Pass" : "Fail";
-
-// console.log(result)
-
-// console.log((10+2)*3+ 6**2)  // 12*3+ 36 // 36 +36
-
-// console.log( "7" = 7)
-
-// let a = 6;
-// let b = a++;   // b=6, a=7
-
-// console.log(a, b);
-
-// console.log(b);
-
-// b = ++a;
-// console.log(b);  //8
-// console.log(a, b);  //8,8
-
-// let a = 4;
-// let b = a++;                //b=6, a=7
-// let c = b++ + ++a;          //4 + 6  =c = 10
-// let d = a++ + ++b;  // 6+ 6  =12
-
-// console.log(a, b, c, d);
-
-// let a = 6;
-// let b = a++;
-
-// console.log(a,b)  // 7,6
-
-// let c = b++ + ++a;   // 6+8=14     b=7 a=8
-// let d = a++ + ++b;  // 8+  8=16      a=9 b=8
-
-// console.log(a, b, c, d);   // 9,8,14,16
-
-// let a=7
-// b= a++  //b=7, a=8
-// console.log(a,b)
-
-// let c= b++ + ++a;   //7 + 9=16   //b=8 a=9
-
-// console.log(a,b,c)
-
-// //9,8,16
-
+let a= 4;
+let b= "4";  
