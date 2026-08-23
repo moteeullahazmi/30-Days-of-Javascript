4b6afd6eefd4f350f7246f02b9aed912f8ccfb87
Moteeullah Azmi <moteeullahazmi@gmail.com>
2026-08-23 10:33:43 +0530

    day 05 complete

--- Day 04 Operators/index.js
+++ Day 04 Operators/index.js
@@ -1,67 +1,57 @@
-// let a =10;
+// let a = 10;
 // let b = 3;
 
-// console.log(a+b);  //13
-// console.log(a - b); // 7
-// console.log(a*b); // 30
-// console.log(a / b); 3.3
-// console.log(a % b); // 1
-// console.log(a ** b); //1000
-
-// let x=5;
-// x +=5; /// x= x+5 > 5+5 = 10
-
-// console.log(10)
+// console.log(a += b);  //
+// console.log(a - b);  // 7
+// console.log(a * b); // 30
+// console.log(a / b);  // 3.3
+// console.log(a % b);  // 1
+// console.log(a ** b); // 1000
 
 // let x = 5;
-// x += 3; //x= x+3  => 5+3 => 8
-// console.log(x); //8
 
-// x = 5;
-// x -= 3;
-// console.log(x); //2
+// console.log((x += 2));
+// console.log((x -= 1)); //6
+// console.log((x *= 3)); // 18
+// console.log(x /= 2) //9
 
-// x = 5;
-// x *= 3;
-// console.log(x); //  15
+// let x= 2;  //number
+// let y= "2";  //string
 
-// x = 5;
-// x /= 3;
-// console.log(x); //1.6
+// console.log(a==b) // true
+// console.log(a === b); //false
 
-// let a = 5;  //number
-// let b = "5";  // string
+// let a = 5;
+// let b = "5";
 
-// console.log(a == b);  //true
+// console.log(a == b);  // true
 // console.log(a === b); // false
-// console.log(a != b);  //false
-// console.log(a !== b);  // true
+// console.log(a != b);  // false
+// console.log(a !== b); //true
 
 // let age = 20;
-// let hasID=false;
+// let hasID = false;
 
-// console.log(age> 18 && hasID);  // true && false => false
+// console.log(age > 18 && hasID);  // true && false => false
+// console.log(age > 18 || hasID);   //true || false => true
+// console.log(!hasID);  //true
 
-// console.log(age>18 || hasID) // true || false => true
+// let a ="5";
+// a++; //6
+// a--;  //5
 
-// console.log(!hasID)  //true
-
-// let a= "-5";
-// a= -a  // 5
-// console.log(a);
 // console.log(typeof a)
 
-// let x= 10;
+// let x = 5;
+
+// x++;
 // x--;
-// console.log(x)
+// console.log(typeof x);
 
-// let age =17;
-// let result = age >=18 ? "Adult" : "Not Adult";
+// let age = 18;
 
-// console.log(result)
+// let result = age > 18 ? "Adult" : "Not Adult";
+// console.log(result);
 
-let result = 2 + 3 * 4; 
+let result = (2 + 3) * 4; // 5*4=> 20
 console.log(result);
-
-let a= 4;
-let b= "4";  
