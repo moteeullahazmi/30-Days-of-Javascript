326b0d77ddbd03b263e4421d51c74572840359a2
Moteeullah Azmi <moteeullahazmi@gmail.com>
2026-08-27 12:41:39 +0530

    day 10 complete

--- Day 10 String/index.js
+++ Day 10 String/index.js
@@ -1,79 +1,45 @@
-// let name = "   JAVASCRIPT     ";
-// // console.log(name.length);
+// console.log("Hello");
+// console.log('Hello');
+// console.log(`Hello`)
 
-// // // upper case
-
-// // console.log(name.toUpperCase())
-// console.log(name.trim());
-
-// let txt= "JavaScript";
-
-// console.log(txt.replace("JavaScript", "Ehtesham"))
-
-// let text = "HTML,CSS,JavaScript";
-// console.log(text.split(","))
-
-// let name = "pushkar"  //k
-// console.log(name.charAt(4))
-
-// let text = "JavaScript";
-
-// console.log(text.indexOf("r"))
-
-// let text="Azmi";
-
-// let azmi = text.toUpperCase();
-
-// console.log(azmi)
-
-// let name1 = "Ehtesham";
-// let name2 = "Pushkar";
-// console.log(name1, "&", name2);
-
-// console.log(`${name1} & ${name2}`);
-
-// let name = "Moteeullah";
-// let age = 25;
-
-// console.log("My name is " + name + " and I am " + age + " years old.");
-
-// console.log(`My name is ${name} and I am ${age} years old `)
+// let name = "Azmi";
+// let city = "Azamgarh"
 
 
+// let text = "Azmi";
+// console.log(text.toLowerCase());
+// // console.log(text.toUpperCase())
+// // console.log(text.length)
 
-// let message = `hello
-// first name is ehtesham
-// second name is pushkar`
-// console.log(message)
-
-
-
-// let a= 10;
-// let b=20;
-// console.log("sum =", a+b)
-
-// console.log(`Sum = ${a+b}`)
+// let text = "lovecoding";
+// console.log(text.slice(0,4)) //index
 
 
-// it's
-
-// console.log("D:\\Youtube>");
-
-// hello
-//azmi
-
-// console.log("hello \njavascript")  //\n for new line
-
-// console.log("hello \t azmi \t pushkar \t ehtesham")  //  t\t tab 
-
-// let array = aljdaslkjfl
-// array[0] = a;
-
-// console.log(array)
+// let text = "I love Java"
+// console.log(text.replace("Java", "JavaScript"))
 
 
+// let name= "Azmi";
+// let msg = "Hello " + name;
+// console.log(msg)
 
-let name  = "     javascript and typescript     "
 
-console.log(name.trim().length)
+// let name = "Azmi";
+// let age = 26;
+// let msg = `Hello my name is ${name} and my age is ${age}`;
+// console.log(msg)
 
+
+// let text = "He said \" Hello\""; //"Hello";
+// console.log(text);
+
+// let newLine = "Hello \tWorld";
+// console.log(newLine);
+
+
+let name = "Azmi";
+let course = "JavaScript"
+
+let message =`Hey ${name}, welcome to ${course}`;
+
+console.log(message)
